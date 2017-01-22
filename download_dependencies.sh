3bd25ec216dce5151cbf40d763f2ea41f41ea078
Amin Benarieb <amin.benarieb@gmail.com>
2017-01-22 11:42:41 +0300

    Fixed dropbox download flags.

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ## Download Caffe Model
-curl -L https://www.dropbox.com/s/05u4e10s0dysg7c/model.caffemodel?dl=0 -o ./model/model.caffemodel
+curl -L https://www.dropbox.com/s/05u4e10s0dysg7c/model.caffemodel?dl=1 -o ./model/model.caffemodel
 
 ## Download OpenCV2 extract it and remove the zipped source
-curl -L0k https://www.dropbox.com/s/2nyxzmacqg71luz/opencv2.framework.zip?dl=0 -o opencv2.framework.zip
+curl -L0k https://www.dropbox.com/s/2nyxzmacqg71luz/opencv2.framework.zip?dl=1 -o opencv2.framework.zip
 unzip opencv2.framework.zip -d TrafficLightApp/
 rm opencv2.framework.zip
